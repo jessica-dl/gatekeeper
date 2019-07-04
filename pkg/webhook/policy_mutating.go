a9644450aee598c93d6ec71b300e2d91365c85ba
Jessica de Leeuw <jessicadl@google.com>
2019-07-04 12:11:07 -0400

    Add a mutating webhook to GK.
    Verified that when GK was deployed on K8s, the log messages were printed
    as expected.
    
    Signed-off-by: Jessica de Leeuw <jessicadl@google.com>

--- pkg/webhook/policy_mutating.go
+++ pkg/webhook/policy_mutating.go
@@ -2,6 +2,10 @@ package webhook
 
 import (
 	"context"
+<<<<<<< HEAD
+	"net/http"
+=======
+>>>>>>> 631d3e2... Add a mutating webhook to GK.
 
 	opa "github.com/open-policy-agent/frameworks/constraint/pkg/client"
 	admissionregistrationv1beta1 "k8s.io/api/admissionregistration/v1beta1"
@@ -18,7 +22,11 @@ func init() {
 	CreateWebhookFuncs = append(CreateWebhookFuncs, AddMutatingWebhook)
 }
 
+<<<<<<< HEAD
 // AddMutatingWebhook creates the mutating webhook
+=======
+// AddMutatingWebhook creates the mutating webhook
+>>>>>>> c6c19e0... Added logs for debugging
 // below: notations add permissions kube-mgmt needs. Access cannot yet be restricted on a namespace-level granularity
 // +kubebuilder:rbac:groups=*,resources=*,verbs=get;list;watch
 // +kubebuilder:rbac:groups=,resources=configmaps,verbs=get;list;watch;create;update;patch;delete
@@ -35,7 +43,7 @@ func AddMutatingWebhook(mgr manager.Manager, opa opa.Client) (webhook.Webhook, e
 				Resources:   []string{"*"},
 			},
 		}).
-		Handlers(&mutationHandler{opa: opa, k8s: mgr.GetClient()}).
+		Handlers(&mutationHandler{opa: opa, k8s: mgr.GetClient(), decoder: mgr.GetAdmissionDecoder()}).
 		WithManager(mgr).
 		Build()
 
@@ -50,20 +58,85 @@ var _ admission.Handler = &mutationHandler{}
 
 type mutationHandler struct {
 	opa     opa.Client
-	k8s  client.Client
+	k8s     client.Client
+	decoder atypes.Decoder
 }
 
 func mutatePods(ctx context.Context, pod *corev1.Pod) error {
-	// do some things
+	log.Info("Trying to mutate pod", "object", pod)
+	if pod.Labels == nil {
+			pod.Labels = map[string]string{}
+	}
+	pod.Labels["someLabel"] = "jessicadl"
 	return nil
 }
 
-// Eventually apply mutations to input objects
+// Apply mutations to input objects
 func (h *mutationHandler) Handle(ctx context.Context, req atypes.Request) atypes.Response {
 	log := log.WithValues("hookType", "mutation")
-	log.Info("(Not) mutating", "object", req.AdmissionRequest.Object)
+	defer log.Info("Finished mutating")
 
-	mResp := admission.ValidationResponse(true, "accept all")
+
+	pod := &corev1.Pod{}
+	_, _, err := deserializer.Decode(req.AdmissionRequest.Object.Raw, nil, pod)
+	if err != nil {
+		log.Info("Decoding failed.", "error", err)
+	  mResp := admission.ValidationResponse(false, "failed to decode")
+		mResp.Response.Result.Code = http.StatusBadRequest
+		return mResp
+	}
+
+	patchObj := pod.DeepCopy()
+
+	err = mutatePods(ctx, patchObj)
+	if err != nil {
+		log.Info("Could not apply mutations.")
+		mResp := admission.ValidationResponse(false, "error applying mutations")
+		mResp.Response.Result.Code = http.StatusInternalServerError
+		return mResp
+	}
+
+	log.Info("mutated pods")
+	mResp := admission.ValidationResponse(true, "successfully mutated pods")
+	mResp = admission.PatchResponse(pod, patchObj)
 
 	return mResp
 }
+
+/*
+func isGkServiceAccount(user authenticationv1.UserInfo) bool {
+	saGroup := fmt.Sprintf("system:serviceaccounts:%s", namespace)
+	for _, g := range user.Groups {
+		if g == saGroup {
+			return true
+		}
+	}
+	return false
+}
+
+// validateGatekeeperResources returns whether an issue is user error (vs internal) and any errors
+// validating internal resources
+func (h *mutationHandler) validateGatekeeperResources(ctx context.Context, req atypes.Request) (bool, error) {
+	if req.AdmissionRequest.Kind.Group == "templates.gatekeeper.sh" && req.AdmissionRequest.Kind.Kind == "MutationTemplate" {
+		return h.validateTemplate(ctx, req)
+	}
+
+	// find out what this is named (mutations.gatekeeper.sh?)
+	if req.AdmissionRequest.Kind.Group == "constraints.gatekeeper.sh" {
+		return h.validateConstraint(ctx, req)
+	}
+
+	return false, nil
+}
+
+func (h *mutationHandler) validateTemplate(ctx context.Context, req atypes.Request) (bool, error) {
+	templ := &templv1alpha1.MutationTemplate{}
+	if _, _, err := deserializer.Decode(req.AdmissionRequest.Object.Raw, nil, templ); err != nil {
+		return false, err
+	}
+	if _, err := h.opa.CreateCRD(ctx, templ); err != nil {
+		return true, err
+	}
+	return false, nil
+}
+*/
